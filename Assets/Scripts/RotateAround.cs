ae5f0b9ddb39df4c45a32d9f562797948991aec2
Gabriel W. Bartmanovicz <obielwb@gmail.com>
2023-11-01 11:19:12 -0300

    feat: create project

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+
+	public Transform target; 
+	public int speed; 
+	
+	void Start() {
+		if (target == null) 
+		{
+			target = this.gameObject.transform;
+			Debug.Log ("RotateAround target not specified. Defaulting to parent GameObject");
+		}
+	}
+	void Update () {
+		transform.RotateAround(target.transform.position,target.transform.up,speed * Time.deltaTime);
+	}
+}
